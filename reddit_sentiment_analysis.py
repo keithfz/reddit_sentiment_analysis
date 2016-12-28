0a48828b10422e9c7955829047c62147ed268890
keithfz <kzeto4@gmail.com>
2016-12-28 16:19:26 -0500

    Update reddit_sentiment_analysis.py

--- reddit_sentiment_analysis.py
+++ reddit_sentiment_analysis.py
@@ -34,9 +34,7 @@ average_polarity = overall_polarity/number_of_sentiments
 # ===============================
 
 
-
 # === Print out data ============
 print('The average subjectivity value is ' + str(average_subjectivity))
 print('The average polarity value is ' + str(average_polarity))
-# ==============================
-=
+# ===============================
